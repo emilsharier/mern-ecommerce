f5a327cb4d715cde8aa940f61ad0e66ea6a4c843
Emil Sharier <emilsharier@gmail.com>
2020-12-10 22:35:49 +0530

    Updating names

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import Layout from './Layout';
-import { getProducts } from './apiCore';
-import Card from './Card';
-import Search from './Search';
-import 'fontsource-roboto';
-import Copyright from './Copyright';
+import React, { useState, useEffect } from "react";
+import Layout from "./Layout";
+import { getProducts } from "./apiCore";
+import Card from "./Card";
+import Search from "./Search";
+import "fontsource-roboto";
+import Copyright from "./Copyright";
 
 const Home = () => {
   const [productsBySell, setProductsBySell] = useState([]);
@@ -12,7 +12,7 @@ const Home = () => {
   const [error, setError] = useState([]);
 
   const loadProductsBySell = () => {
-    getProducts('sold').then((data) => {
+    getProducts("sold").then((data) => {
       if (data.error) {
         setError(data.error);
       } else {
@@ -22,7 +22,7 @@ const Home = () => {
   };
 
   const loadProductsByArrival = () => {
-    getProducts('createdAt').then((data) => {
+    getProducts("createdAt").then((data) => {
       if (data.error) {
         setError(data.error);
       } else {
@@ -38,33 +38,33 @@ const Home = () => {
 
   return (
     <Layout
-      title='Home page'
-      description='MERN E-commerce App'
-      className='container-fluid'
+      title="Home page"
+      description="QuickKart"
+      className="container-fluid"
     >
       <Search />
-      <div className='row'>
-        <div className='col-md-1'></div>
-        <div className='col-md-10'>
-          <h2 className='mb-2'>New Arrivals</h2>
-          <div className='row'>
+      <div className="row">
+        <div className="col-md-1"></div>
+        <div className="col-md-10">
+          <h2 className="mb-2">New Arrivals</h2>
+          <div className="row">
             {productsByArrival.map((product, i) => (
-              <div key={i} className='col-md-4 col-sm-12'>
+              <div key={i} className="col-md-4 col-sm-12">
                 <Card product={product} />
               </div>
             ))}
           </div>
 
-          <h2 className='mb-2 mt-4'>Best Sellers</h2>
-          <div className='row'>
+          <h2 className="mb-2 mt-4">Best Sellers</h2>
+          <div className="row">
             {productsBySell.map((product, i) => (
-              <div key={i} className='col-md-4 col-sm-12'>
+              <div key={i} className="col-md-4 col-sm-12">
                 <Card product={product} />
               </div>
             ))}
           </div>
         </div>
-        <div className='col-md-1'></div>
+        <div className="col-md-1"></div>
       </div>
 
       <Copyright />
